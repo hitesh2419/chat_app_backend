a65da6bb324f6b5eafe1752781b691d736b577a7
Hitesh Saini <hiteshsaini57520@gmail.com>
2023-02-23 22:19:05 +0530

    some changes done in cloudinary

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dscw451yw",
-  api_key: "212277687815232",
-  api_secret: "4EG2C_oMHWpC3_yX_hsVTzDmTIA",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key:  process.env.API_KEY,
+  api_secret:  process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
