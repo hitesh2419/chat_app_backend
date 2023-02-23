69aefef41136bc7923688fb4a3443faa192b800a
Hitesh Saini <hiteshsaini57520@gmail.com>
2023-02-23 23:09:36 +0530

    database connection changed

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 require("dotenv").config();
-const app = express();
 require("./server/db")
-const port = process.env.PORT || 5000;
-
+const app = express();
 const usersRoute = require("./server/routes/usersRoute");
 const chatsRoute = require("./server/routes/chatsRoute");
 const messagesRoute = require("./server/routes/messagesRoute");
@@ -12,7 +10,7 @@ app.use(
     limit: "50mb",
   })
 );
-
+const port = process.env.PORT || 5000;
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Mongo DB Connection Successfull");
   })
