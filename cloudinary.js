84f30468fa82e01b68028c254d6826b27c5753cf
Hitesh Saini <hiteshsaini57520@gmail.com>
2023-02-23 21:10:05 +0530

    chat backend completed

---
+++ cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dscw451yw",
+  api_key: "212277687815232",
+  api_secret: "4EG2C_oMHWpC3_yX_hsVTzDmTIA",
+});
+
+module.exports = cloudinary;
